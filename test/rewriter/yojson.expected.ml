dde819c74613397df8890c7f6c7fc6430d923bbd
Sonja Heinze <sonjaleaheinze@gmail.com>
2022-04-21 21:29:38 +0200

    Update tests to ppxlib.0.26.0
    ppxlib.0.26.0 has bumped the AST to 4.14 and with that has changed
    Pprintast.
    
    Signed-off-by: Sonja Heinze <sonjaleaheinze@gmail.com>

--- test/rewriter/yojson.expected.ml
+++ test/rewriter/yojson.expected.ml
@@ -32,18 +32,18 @@ let patterns =
     | `Int 1 as _int -> ()
     | `Intlit "4611686018427387904" as _int_lit -> ()
     | `Float 1.2e+10 as _float -> ()
-    | `Int 1|`Int 2 as _or_pattern -> ()
+    | `Int 1 | `Int 2 as _or_pattern -> ()
     | `Int 1 as _s as _alias -> ()
     | `List ((`Int 1)::(`Int 2)::(`Int 3)::[]) as _array -> ()
     | `List ((`Bool (true))::(`Int 1)::(`String "a")::[]) as _mixed_array ->
         ()
     | `Assoc (("a", `Bool (true))::("b", `Int 1)::("c", `String "a")::[])
-      |`Assoc (("a", `Bool (true))::("c", `String "a")::("b", `Int 1)::[])
-      |`Assoc (("b", `Int 1)::("a", `Bool (true))::("c", `String "a")::[])
-      |`Assoc (("b", `Int 1)::("c", `String "a")::("a", `Bool (true))::[])
-      |`Assoc (("c", `String "a")::("a", `Bool (true))::("b", `Int 1)::[])
-      |`Assoc (("c", `String "a")::("b", `Int 1)::("a", `Bool (true))::[]) as
-        _object -> ()
+      | `Assoc (("a", `Bool (true))::("c", `String "a")::("b", `Int 1)::[])
+      | `Assoc (("b", `Int 1)::("a", `Bool (true))::("c", `String "a")::[])
+      | `Assoc (("b", `Int 1)::("c", `String "a")::("a", `Bool (true))::[])
+      | `Assoc (("c", `String "a")::("a", `Bool (true))::("b", `Int 1)::[])
+      | `Assoc (("c", `String "a")::("b", `Int 1)::("a", `Bool (true))::[])
+        as _object -> ()
     | `Assoc
         (("description", `String "Some written thing")::("authors",
                                                          `List
@@ -52,39 +52,39 @@ let patterns =
                                                                  _kurt)::
                                                                ("age",
                                                                 `Int 27)::[])
-                                                             |`Assoc
-                                                                (("age",
-                                                                  `Int 27)::
-                                                                ("name",
-                                                                 _kurt)::[]))::(
+                                                             | `Assoc
+                                                                 (("age",
+                                                                   `Int 27)::
+                                                                 ("name",
+                                                                  _kurt)::[]))::(
                                                            `Assoc
                                                              (("name",
                                                                `String
                                                                  "Jesus Christ")::
                                                              ("age", _)::[])
-                                                           |`Assoc
-                                                              (("age", _)::
-                                                              ("name",
-                                                               `String
-                                                                 "Jesus Christ")::[]))::[]))::[])
-      |`Assoc
-         (("authors",
-           `List
-             ((`Assoc (("name", _kurt)::("age", `Int 27)::[])
-               |`Assoc (("age", `Int 27)::("name", _kurt)::[]))::(`Assoc
-                                                                    (
-                                                                    ("name",
+                                                           | `Assoc
+                                                               (("age", _)::
+                                                               ("name",
+                                                                `String
+                                                                  "Jesus Christ")::[]))::[]))::[])
+      | `Assoc
+          (("authors",
+            `List
+              ((`Assoc (("name", _kurt)::("age", `Int 27)::[])
+                | `Assoc (("age", `Int 27)::("name", _kurt)::[]))::(`Assoc
+                                                                    (("name",
                                                                     `String
                                                                     "Jesus Christ")::
                                                                     ("age",
                                                                     _)::[])
-                                                                  |`Assoc
+                                                                    | 
+                                                                    `Assoc
                                                                     (("age",
                                                                     _)::
                                                                     ("name",
                                                                     `String
                                                                     "Jesus Christ")::[]))::[]))::
-         ("description", `String "Some written thing")::[])
+          ("description", `String "Some written thing")::[])
         as _complex -> ()
     | `Intlit "1" as _int_64 -> ()
     | `Intlit "1" as _int_32 -> ()
